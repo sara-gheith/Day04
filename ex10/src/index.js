0f4511e29e2163bcd71c801207b3f0bfad3c2953
sara-gheith <saraannegheith@gmail.com>
2021-03-11 16:54:47 +0100

    Declaring a String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Sara";
+    var myLastName = "Gheith";
+    var myHomeTown = "Salzburg";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
